1f6184fb1130bb0d7e9a3ab6bec5ab154bc99ef9
apolzin <alexander@unloved.at>
2009-02-18 14:35:10 +0100

    makes shadows aware if its already initialized

--- init.rb
+++ init.rb
@@ -1,9 +1,21 @@
-require File.dirname(__FILE__) + "/lib/shadows"
-require File.dirname(__FILE__) + "/lib/shadows/base"
-
 default_load_path = File.join Rails.root, %w[ app shadows ]
-shapes_load_path = File.join Rails.root, %w[ vendor plugins shapes app shadows ]
-Shadows::Base.load_paths = [ default_load_path, shapes_load_path ]
+additional_load_path = File.join Rails.root, %w[ vendor plugins shapes app shadows ]
 
-ActiveRecord::Base.extend Shadows::Extension rescue nil
-ActionController::Base.around_filter Shadows::Filter rescue nil
+# if shadows is already defined just include additional_load_path
+# else do the whole plugin initialisation
+
+if defined? Shadows::Base
+  Shadows::Base.load_paths << additional_load_path
+else
+  require File.dirname(__FILE__) + "/lib/shadows"
+  require File.dirname(__FILE__) + "/lib/shadows/base"
+  Shadows::Base.load_paths = [ default_load_path, additional_load_path ]
+  ActiveRecord::Base.extend Shadows::Extension rescue nil
+  ActionController::Base.around_filter Shadows::Filter rescue nil
+  ActiveRecord::Base.extend Shadows::Extension rescue nil
+  ActionController::Base.around_filter Shadows::Filter rescue nil
+end
+
+
+
+
